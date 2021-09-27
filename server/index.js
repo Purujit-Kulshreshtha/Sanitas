864771737d3a365ed0f300490e6133f2c3e4df84
Purujit-Kulshreshtha <68121424+Purujit-Kulshreshtha@users.noreply.github.com>
2021-09-27 05:28:19 +0000

    Update index.js

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ app.use(cors())
 app.use('/user', userRouter)
 app.use('/request', requestRouter)
 
-const databaseURI = "mongodb+srv://Purujit:sanitasPassword@sanitascluster.l9jds.mongodb.net/SanitasCluster?retryWrites=true&w=majority"
+const databaseURI = process.env.URI
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(databaseURI, {useNewUrlParser: true, useUnifiedTopology: true}).then(() => {
